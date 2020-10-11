7b988190a4c46faad91103ae9c6611e5dcf9193d
Dashpilot <39771945+dashpilot@users.noreply.github.com>
2020-10-11 04:39:47 +0200

    initial version

--- api/auth.js
+++ api/auth.js
@@ -1,15 +1,16 @@
-const fetch = require('node-fetch')
+const fetch = require("node-fetch");
 
 module.exports = (req, res) => {
+    const token = req.header("authorization");
 
-  const token = req.header('authorization');
+    fetch("https://v1.userbase.com/v1/admin/auth-tokens/" + token, {
+            headers: {
+                Authorization: "Bearer " + process.env.UB_KEY,
+            },
+        })
+        .then((res) => res.json())
+        .then((json) => console.log(json))
+        .catch((err) => console.error(err));
 
-  fetch("https://v1.userbase.com/v1/admin/auth-tokens/"+token, {
-    headers: {
-      Authorization: "Bearer "+process.env.UB_KEY
-    }
-  })
-
-  res.send(`Hello ${name}!`)
-
-}
+    res.send(`Hello world`);
+};
